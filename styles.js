6b448f804125234605560044efb288833f9bd77a
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-10 10:38:35 +0300

    added loading component.

---
+++ styles.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export const StyledView = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  height: 20%;
+`;
