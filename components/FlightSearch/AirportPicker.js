02079dbc11e2344e20a959c30ea742bbfd3d6742
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-10 18:43:32 +0300

    Flight search ammendments.

---
+++ components/FlightSearch/AirportPicker.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Picker, Icon } from "native-base";
+const AirportPicker = ({ filter, setFilter, items, type }) => {
+  return (
+    <Picker
+      mode="dropdown"
+      iosIcon={<Icon name="arrow-down" />}
+      placeholder="Select your departure airport"
+      placeholderStyle={{ color: "#bfc6ea" }}
+      placeholderIconColor="#007aff"
+      // style={{ width: 200 }}
+      selectedValue={filter[type]}
+      onValueChange={(airport) => setFilter({ ...filter, [type]: airport })}
+    >
+      {items}
+    </Picker>
+  );
+};
+
+export default AirportPicker;
---
+++ components/FlightSearch/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+import DatePicker from "react-native-datepicker";
+import { Container } from "native-base";
+
+export const StyledContainer = styled(Container)`
+  flex: 1;
+  align-self: stretch;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f6ff;
+  padding-right: 60px;
+  padding-left: 60px;
+  margin-top: 40px;
+`;
