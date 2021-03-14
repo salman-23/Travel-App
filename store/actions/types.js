042690720365183c8cd6ea6e3ef2323bf5240c15
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-14 16:41:41 +0300

    flight store and types changes

--- store/actions/types.js
+++ store/actions/types.js
@@ -1,8 +1,10 @@
 //User
 export const SET_USER = "SET_USER";
-export const FETCH_PROFILE = 'FETCH_PROFILE';
-export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+export const FETCH_PROFILE = "FETCH_PROFILE";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
 //Destinations
 export const FETCH_DESTINATIONS = "FETCH_DESTINATIONS";
 //Flights
-export const SEARCH_FLIGHT = "SEARCH_FLIGHT";
+export const FETCH_FLIGHTS = "FETCH_FLIGHTS";
+//BOOKING
+export const CREATE_BOOKING = "CREATE_BOOKING";
