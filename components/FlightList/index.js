0c090b7271ca89d07f093bc08a41304e39386d99
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-14 16:42:34 +0300

    flightlist changes

--- components/FlightList/index.js
+++ components/FlightList/index.js
@@ -1,21 +1,60 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { List, Container, Content } from "native-base";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 //Components
 import Loading from "../Loading";
 import FlightItem from "./FlightItem";
+import { bookingCreate } from "../../store/actions/bookingActions";
+//Styling
+import { List, Container, Content, Button, Text } from "native-base";
 
-const FlightList = () => {
-  const { flights, loading } = useSelector((state) => state.flightReducer);
+const FlightList = ({ navigation }) => {
+  const { flights, loading, returnFlights } = useSelector(
+    (state) => state.flightReducer
+  );
+
+  const dispatch = useDispatch();
+  const [selectedFlight, setSelectedFlight] = useState(null);
 
   if (loading) return <Loading />;
 
-  const flightList = flights.map((flight) => <FlightItem flight={flight} />);
+  const handleSelect = (flightId) => setSelectedFlight(flightId);
+  const handleSubmit = () => {
+    dispatch(
+      bookingCreate(
+        selectedFlight,
+        navigation,
+        returnFlights.length,
+        "FlightList"
+      )
+    );
+    handleSelect(null);
+  };
+  const flightList = flights.map((flight) => (
+    <FlightItem
+      flight={flight}
+      selectedFlight={selectedFlight}
+      handleSelect={handleSelect}
+    />
+  ));
 
   return (
     <Content>
       <Container>
-        <List>{flightList}</List>
+        <List>
+          {flightList}
+          {selectedFlight && (
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onPress={handleSubmit}
+            >
+              <Text>{returnFlights.length ? "Next" : "Book"}</Text>
+            </Button>
+          )}
+        </List>
       </Container>
     </Content>
   );
--- components/FlightList/styles.js
+++ components/FlightList/styles.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
-import { Thumbnail } from "native-base";
+import { Thumbnail, ListItem, Text } from "native-base";
 
 export const ThumbnailStyled = styled(Thumbnail)`
   width: 50px;
   height: 50px;
   margin-bottom: 10px;
+`;
+
+export const ListItemStyled = styled(ListItem)`
+  margin-bottom: 2%;
+  background: ${(props) => (props.selected ? "#90ee90" : "white")};
+`;
+
+export const StyledMessage = styled(Text)`
+  font-size: 20px;
+  text-align: center;
+  margin: auto;
 `;
