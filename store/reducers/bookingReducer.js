c3ad07dca6a7de0d3a7edfd2a45606d8817a8c0f
Mahmood28 <mahmoodalwatani@gmail.com>
2021-03-16 10:49:54 +0300

    booking and flight store

--- store/reducers/bookingReducer.js
+++ store/reducers/bookingReducer.js
@@ -1,20 +1,38 @@
-import { CREATE_BOOKING } from "../actions/types";
-
+import { CREATE_BOOKING, SET_DETAILS, SET_FLIGHTS } from "../actions/types";
 const initialState = {
-  bookedFlights: [],
-  bookingLoading: true,
+  bookings: [],
+  flights: {
+    departing: null,
+    returning: null,
+  },
+  passengersCount: 0,
+  travelClassId: 0,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_BOOKING:
-      const { flightId } = action.payload;
+      const { flights, newBooking } = action.payload;
       return {
         ...state,
-        bookedFlights: [...state.bookedFlights, flightId],
-        bookingLoading: false,
+        bookings: [...state.bookings, newBooking],
       };
-
+    case SET_DETAILS:
+      const { passengersCount, travelClassId } = action.payload;
+      return {
+        ...state,
+        passengersCount,
+        travelClassId,
+      };
+    case SET_FLIGHTS:
+      const { departing, returning } = action.payload;
+      return {
+        ...state,
+        flights: {
+          departing: departing ?? state.flights.departing,
+          returning: returning ?? state.flights.returning,
+        },
+      };
     default:
       return state;
   }
